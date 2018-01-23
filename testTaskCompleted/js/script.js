570d04d940caf2f63ba24cc142c8222b8aadcc10
Ivan Skokowski <ivanskokowski@gmail.com>
2018-01-23 12:33:51 +0200

    Signed-off-by: Ivan Skokowski <ivanskokowski@gmail.com>

---
+++ testTaskCompleted/js/script.js
@@ -0,0 +1,12 @@
+/*
+(function () {    //IIFE
+    "use strict";
+
+    function loaded() {
+        var myScroll = new IScroll('#articles', {mouseWheel: true});
+    }
+
+    window.onload = function (ev) {
+
+    }
+}());*/
